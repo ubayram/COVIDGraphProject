157e0f55d9c9f228d37204852ea8647d4b018aca
Lamia Ben <benhiba.lamia@gmail.com>
2020-06-24 23:30:38 +0100

    NER and nighbors Entity and link extraction
    Include only scispacy recognized entities and their immediate neighbors.

--- save_nodes_per_text.py
+++ save_nodes_per_text.py
@@ -2,7 +2,7 @@
 # ulyabayram@gmail.com
 #
 
-import subject_verb_object_extract_LB_0906_scispacy as sci # get Lamia's code
+import subject_verb_object_extract_LB_2406_scispacy as sci # get Lamia's code
 import spacy
 import en_core_web_sm
 import pandas as pd
@@ -15,7 +15,7 @@ import nltk
 # use spacy small model
 nlp = en_core_web_sm.load()
 
-path_to_stopwords = 'nltk_stopwords.txt'
+path_to_stopwords = './nltk_brown_stopwords.txt'
         
 # Keep this block
 with open(path_to_stopwords, 'r') as file:
@@ -43,7 +43,10 @@ def collectSaveEdges(df, pd_eval, row_indices, date_flag):
         for curr_sent in list_sentences:
             if len(curr_sent.split(' ')) > 3: # make sure a sentence has at least 4 words - to eliminate noise
                 #print('\n'+curr_sent)
-                curr_tuples += sci.extract_link(nlp(curr_sent))
+                links = sci.extract_link(nlp(curr_sent))
+                if len(links):
+                    print(links)
+                    curr_tuples += links
 
         #t.stop()
         pd_eval = pd_eval.append(pd.DataFrame({'filename' : df.fullname[i_row], 'list_of_edges': [curr_tuples], 'timestamp': curr_date}))
@@ -54,7 +57,7 @@ def collectSaveEdges(df, pd_eval, row_indices, date_flag):
 if __name__ == '__main__':
 
     # read the corpus
-    all_data = pd.read_csv('full_cord19_texts.csv')
+    all_data = pd.read_csv('./data/full_cord19_texts.csv')
 
     print('Processing before 2020, first half')
     # list of rows where years are before 2020
@@ -64,7 +67,7 @@ if __name__ == '__main__':
     empty_list = []
     pd_eval = pd.DataFrame(data={'filename' : empty_list, 'list_of_edges': empty_list, 'timestamp': empty_list})
     pd_eval = collectSaveEdges(all_data, pd_eval, row_indices_pre, False)
-    pd_eval.to_csv('list_of_edges_pre2020_first_half.csv')
+    pd_eval.to_csv('list_of_edges_pre2020_first_half_new.csv')
     del pd_eval
 
     '''
