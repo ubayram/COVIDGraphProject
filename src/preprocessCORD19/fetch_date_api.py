7aa818e741bfb3de6032083d828b6c1bb09aff0c
Ulya Bayram <ulyabayram@gmail.com>
2021-03-02 13:27:17 +0300

    Update fetch_date_api.py

--- src/preprocessCORD19/fetch_date_api.py
+++ src/preprocessCORD19/fetch_date_api.py
@@ -1,5 +1,36 @@
-# Lamia Ben hiba
-# lamia.benhiba@gmail.com
+#Author: Lamia Ben hiba
+#email : lamia.benhiba@gmail.com
+#
+#------------------------------------------------------------------------------------------------------
+#
+#The content of this project is licensed under the MIT license. 2021 All rights reserved.
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+#and associated documentation files (the "Software"), to deal with the Software without restriction, 
+#including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+#and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+#subject to the following conditions:
+#
+#Redistributions of source code must retain the above License notice, this list of conditions and 
+#the following disclaimers.
+#
+#Redistributions in binary form must reproduce the above License notice, this list of conditions and 
+#the following disclaimers in the documentation and/or other materials provided with the distribution. 
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+#LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+#IN NO EVENT SHALL THE CONTRIBUTORS OR LICENSE HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+#WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+#OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+#
+#------------------------------------------------------------------------------------------------------
+#
+#These code are writen for a research project, published in OIR. If you use any of them, please cite:
+
+#Ulya Bayram, Runia Roy, Aqil Assalil, Lamia Ben Hiba, 
+#"The Unknown Knowns: A Graph-Based Approach for Temporal COVID-19 Literature Mining", 
+#Online Information Review (OIR), COVID-19 Special Issue, 2021.
+#
+#------------------------------------------------------------------------------------------------------
 # Enriching the metadata with missing dates
 
 import pandas as pd
